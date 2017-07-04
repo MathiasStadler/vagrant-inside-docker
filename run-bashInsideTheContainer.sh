e1e472055f3c4f1c0d775cf17b8e2068ac87fe3e
MathiasStadler <stadler-mathias@web.de>
2017-07-04 22:59:40 +0200

    add --cidfile and usage script

---
+++ run-bashInsideTheContainer.sh
@@ -0,0 +1,1 @@
+docker exec -it $(cat /tmp/vagrant-inside-docker.cid) bash
--- run-fromGitHubRepo.sh
+++ run-fromGitHubRepo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -it mathiasstadler/vagrant-inside-docker:latest /bin/bash -c "vagrant up --provider virtualbox && vagrant ssh"
+docker run  --cidfile /tmp/vagrant-inside-docker.cid --name "vagrant-inside-docker" --privileged -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl  -it mathiasstadler/vagrant-inside-docker:latest /bin/bash -c "vagrant up --provider virtualbox && vagrant ssh"  
